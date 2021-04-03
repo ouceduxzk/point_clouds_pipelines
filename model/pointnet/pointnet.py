a4ff25c63b3dff6ed5644cb8b58b5cf1b97531c8
zaikun xu <zaikun.xu@astrivis.com>
2021-04-03 22:26:22 +0200

    update

--- model/pointnet/pointnet.py
+++ model/pointnet/pointnet.py
@@ -85,7 +85,7 @@ class PointNetCls(nn.Module):
   def __init__(self, feature_transform, num_classes):
     super(PointNetCls, self).__init__()
     self.num_classes = num_classes
-    self.feat, trans = PointNetFeature()
+    self.feat = PointNetFeature()
     self.fc1 = nn.Linear(1024, 512)
     self.fc2 = nn.Linear(512, 256)
     self.fc3 = nn.Linear(256, self.num_classes)
@@ -95,7 +95,7 @@ class PointNetCls(nn.Module):
     self.bn2 = nn.BatchNorm1d(256)
 
   def forward(self, x):
-    x = self.feat(x)
+    x, trans = self.feat(x)
     x = F.relu(self.bn1(self.fc1(x)))
     x = self.dropout(self.fc2(x))
     x = F.relu(self.bn2(x)())
